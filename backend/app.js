ac0e738d2ddcc3fecc3086b0ce89fe57cd5d8a0d
Adel Hosseini [AH1004_SA] <AH1004_SA@healthbc.org>
2022-01-26 16:03:59 -0800

    completing the post requests waiting to connect the db

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,29 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
+  next();
+});
+
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body
+  console.log(post);
+  res.status(201).json({
+    message: 'post added successfully'
+  })
+
+} )
+app.get("/api/posts", (req, res, next) => {
   const posts =[
     {
       id: "fds2343fsdf",
